26a17008d65bbb00d5c9600454410d653eb9a3f7
Tjaž Vračko <tjaz.vracko@gmail.com>
2018-11-03 11:52:37 +0100

    Work on authController

--- VNAST_api/api/controllers/authController.js
+++ VNAST_api/api/controllers/authController.js
@@ -27,6 +27,7 @@ exports.register_a_user =  function(req, res) {
     }); 
 };
 
+// TODO: projection nad passwordom nimamo več - to je slabo, naredi tak kot je blo prej
 exports.who_am_i = function(req, res) {
     // var token = req.headers['x-access-token'];
     // if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
