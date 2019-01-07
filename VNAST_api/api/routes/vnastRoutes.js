8f263c81306c7262eb95d436b10e029949db63d6
Tjaž Vračko <tjaz.vracko@gmail.com>
2019-01-07 12:44:17 +0100

    Added ApiDocs & generator script

--- VNAST_api/api/routes/vnastRoutes.js
+++ VNAST_api/api/routes/vnastRoutes.js
@@ -14,9 +14,66 @@ module.exports = function(app) {
     var validate = require('./validation');
     
     // Authentication
+    /**
+     * @api {post} /register Register new user
+     * @apiName RegisterUser
+     * @apiGroup Authentication
+     *
+     * @apiParam {String} username Users username.
+     * @apiParam {String} password Users password.
+     * @apiParam {String='worker', 'manager', 'admin'} privilege Users privilege level.
+     * @apiParam {String} [email] Users Email.
+     *
+     * @apiSuccess {Boolean} auth Success of authentication.
+     * @apiSuccess {String} token JSON Web token used for authentication/authorisation.
+     * @apiSuccessExample {json} Success Response Example:
+                   {
+                        "auth": true,
+                        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViZDk4OWQ4MTc0NjY4M2IyZDg1MTFmNiIsImlhdCI6MTU0Njg2MDM5NCwiZXhwIjoxNTQ2OTQ2Nzk0fQ.IYaMtOVonFUHJOZ4ZvyhdE3diiMMlFMmXBG42MXQhwc"
+                    }
+     * 
+     */
     app.post('/register', validate.register, authController.register_a_user);
-    app.get('/me', verify.token, authController.who_am_i);
+    
+    /**
+     * @api {post} /login Login existing user
+     * @apiName LoginUser
+     * @apiGroup Authentication
+     *
+     * @apiParam {String} username Users username.
+     * @apiParam {String} password Users password.
+     *
+     * @apiSuccess {Boolean} auth Success of authentication.
+     * @apiSuccess {String} token JSON Web token used for authentication/authorisation.
+     * * @apiSuccessExample {json} Success Response Example:
+                   {
+                        "auth": true,
+                        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViZDk4OWQ4MTc0NjY4M2IyZDg1MTFmNiIsImlhdCI6MTU0Njg2MDM5NCwiZXhwIjoxNTQ2OTQ2Nzk0fQ.IYaMtOVonFUHJOZ4ZvyhdE3diiMMlFMmXBG42MXQhwc"
+                    }
+     * 
+     */
     app.post('/login', authController.login_a_user);
+    
+    /**
+     * @api {get} /me Return users info based on Auth token
+     * @apiName GetSelf
+     * @apiGroup Authentication
+     *
+     * @apiHeader {String} x-access-token Auth token.
+     *
+     * @apiSuccess {String[]} privilege Privilege level of user.
+     * @apiSuccess {String} username Username of the user.
+     * @apiSuccess {String} email Email of the user.
+     * @apiSuccessExample {json} Success Response Example:
+                   {
+                        "privilege": ["admin"],
+                        "_id": "5bd989d81746683b2d8511f6",
+                        "username": "admin",
+                        "email": "admin.admin@admin.sem",
+                    }
+     */
+    app.get('/me', verify.token, authController.who_am_i);
+
     app.get('/logout', authController.logout_a_user); //nepotrebno, a tukaj kot prikaz logike - za logout enostavno zbrišemo token client-side (po 24h pa itak treba ponovni login ker token expire-a)
 
     // Users
